eff5be134f1f896075069d1bd8709d7906f0da7c
Hemant <davidbroddy1@gmail.com>
2024-08-03 19:02:09 +0530

    "Removed commented out old code for createMessage function "

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -4,28 +4,6 @@ import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 // create message
-// const createMessage = asyncHandler(
-//   async ({ sender, receiver, content, ride }) => {
-//     if (!sender || !receiver || !content) {
-//       console.log({ sender, receiver, content, ride });
-
-//       throw new ApiError(400, "Sender, reciever & content is required");
-//     }
-
-//     try {
-//       return await Message.create({
-//         sender,
-//         receiver,
-//         content,
-//         ride,
-//       });
-//     } catch (error) {
-//       console.log(error);
-//       throw new ApiError(500, "failed to create message");
-//     }
-//   }
-// );
-
 export const createMessage = async ({ sender, receiver, content, ride }) => {
   try {
     const message = await Message.create({
