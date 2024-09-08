3789647bcbcf8d51d46a71b49a710db7d74826f8
Hemant <davidbroddy1@gmail.com>
2024-09-08 11:50:42 +0530

    "Updated RideHistory component with new UI and added functionality to navigate to ride details page on click"

--- client/src/components/ride/RideHistory.jsx
+++ client/src/components/ride/RideHistory.jsx
@@ -1,21 +1,60 @@
+import { getRideDetails } from "@/actions/ride.action";
 import React from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const RideHistory = () => {
+  const navigate = useNavigate();
   const { ridesHistory } = useSelector((state) => state.user.user);
+
+  const handleRideClick = (id) => {
+    if (id) {
+      getRideDetails(id);
+      navigate(`/ride/${id}`);
+    } else {
+      console.log("Ride id not found");
+    }
+  };
+
   return (
     <>
-      <div className="mt-4">
-        <h3 className="text-lg font-medium text-gray-800">Ride History</h3>
-        <ul className="mt-2 text-gray-600">
-          {ridesHistory.length > 0 ? (
-            ridesHistory.map((ride, index) => (
-              <li key={index}>Ride ID: {ride}</li>
-            ))
-          ) : (
-            <p>No rides yet.</p>
-          )}
-        </ul>
+      <div className="m-5">
+        <h2 className="text-2xl font-semibold">Rides History</h2>
+        <div className="mt-5">
+          <div className="mt-3">
+            {ridesHistory.length > 0 ? (
+              <div className="grid grid-cols-1 gap-4 cursor-pointer">
+                {ridesHistory.map((ride) => (
+                  <div
+                    key={ride._id}
+                    className="p-4 border rounded-lg shadow-md md:flex justify-between"
+                    onClick={() => handleRideClick(ride.rideId)}
+                  >
+                    <div>
+                      <p className="">
+                        From:{" "}
+                        <span className="text-gray-400">
+                          {ride.source.name}
+                        </span>
+                      </p>
+                      <p className="">
+                        To:{" "}
+                        <span className="text-gray-400">
+                          {ride.destination.name}
+                        </span>
+                      </p>
+                    </div>
+                    <p className="text-gray-500">
+                      {new Date(ride.departureTime).toDateString()}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p>No rides published yet.</p>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
