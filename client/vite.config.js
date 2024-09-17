edfbae8669fdb3e88df8a18cf3091054b9383ec3
Hemant <davidbroddy1@gmail.com>
2024-09-17 10:34:26 +0530

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://wheel-share.onrender.com",
         changeOrigin: true,
         secure: false,
       },
