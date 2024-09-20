ba0aa6303c45f37a7f213358147a424bcefe066f
Hemant <davidbroddy1@gmail.com>
2024-09-20 12:59:52 +0530

    "Added import for fileURLToPath and updated __dirname to support ES modules"

--- server/src/app.js
+++ server/src/app.js
@@ -38,7 +38,12 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 import errorHandler from "./utils/errorHandler.js";
+
+// Fix __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
